924ed56f535a4cd2d90c5eae93f68ec31648b8e2
Heitor Tancredo <heitortancredo@gmail.com>
2019-04-25 13:35:22 -0300

    using asyncio

--- part-2/src/agregador.py
+++ part-2/src/agregador.py
@@ -3,14 +3,14 @@ import json
 import requests
 import time
 from pprint import pprint
+import aiohttp
+import asyncio
 
 cached = {}
 
-def unique_urls(product_list):
-    unique = [dict(t) for t in set(tuple(sorted(d.items())) for d in product_list)]
-    return unique
+http_client = aiohttp.ClientSession()
 
-def split_list(product_list):
+async def split_list(product_list):
     result = {}
 
     for prod in product_list:
@@ -18,18 +18,19 @@ def split_list(product_list):
         _img = prod['image']
 
         try:
-            # 3 images per product
+            # 3 valid url per product
             if len(result[_id]) < 3:
-                valid = check_url(_img)
+                valid = await check_url(_img)
                 if valid:
                     result[_id].append(_img)
         except KeyError:
-            result[_id] = []
-            valid = check_url(_img)
+            valid = await check_url(_img)
             if valid:
+                result[_id] = []
                 result[_id].append(_img)
 
     return result
+
 
 def etl(result_list):
 
@@ -43,34 +44,37 @@ def etl(result_list):
 
     return final_list
 
-
-def check_url(url):
+#  TODO: make async requests
+async def check_url(url):
     valid = False
 
     if url in cached:
         return cached[url]
 
-    r = requests.get(url)
-    if r.status_code < 300:
+    r = await http_client.get(url)
+    if r.status < 300:
         valid = True
 
     cached[url] = valid
     return valid
 
-def main():
+async def main():
 
-    # start = time()
+    start = time.time()
+
     file_in = sys.argv[1]
 
     dict_out = [json.loads(line.rstrip('\n')) for line in open(file_in)]
 
-    # unique = unique_urls(dict_out)
-    o = split_list(dict_out)
+    o = await split_list(dict_out)
+    await http_client.close()
     final = etl(o)
 
-    # end = time()
-    # print(end-start)
     pprint(final)
+    end = time.time()
+
+    print(f'Elapsed time: {end-start}')
 
 if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
