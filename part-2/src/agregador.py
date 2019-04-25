fa7d90a86e318c67f3b325483f7a7741e6255ae5
Heitor Tancredo <heitortancredo@gmail.com>
2019-04-25 14:45:51 -0300

    Code refactoring

--- part-2/src/agregador.py
+++ part-2/src/agregador.py
@@ -6,71 +6,83 @@ from pprint import pprint
 import aiohttp
 import asyncio
 
-cached = {}
 
-http_client = aiohttp.ClientSession()
+class Aggregate:
 
-async def split_list(product_list):
-    result = {}
+    def __init__(self):
+        self.cached = {}
 
-    for prod in product_list:
-        _id = prod['productId']
-        _img = prod['image']
+        self.http_client = aiohttp.ClientSession()
 
-        try:
-            # 3 valid url per product
-            if len(result[_id]) < 3:
-                valid = await check_url(_img)
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *excinfo):
+        await self.http_client.close()
+
+    async def sanitize(self, product_list):
+        result = {}
+
+        for prod in product_list:
+            _id = prod['productId']
+            _img = prod['image']
+
+            try:
+                # 3 valid url per product
+                if len(result[_id]) < 3:
+                    valid = await self.__check_url(_img)
+                    if valid:
+                        result[_id].append(_img)
+            except KeyError:
+                valid = await self.__check_url(_img)
                 if valid:
+                    result[_id] = []
                     result[_id].append(_img)
-        except KeyError:
-            valid = await check_url(_img)
-            if valid:
-                result[_id] = []
-                result[_id].append(_img)
 
-    return result
+        return self.__transforming(result)
+
+    def dump(self, result_list, dump_file):
+        with open(dump_file, 'a') as fd_out:
+            fd_out.writelines(f"{x}\n" for x in result_list)
 
 
-def etl(result_list):
+    def __transforming(self, result_list):
 
-    final_list = []
+        final_list = []
 
-    for pid in result_list.keys():
-        product = {}
-        product['productId'] = pid
-        product['images'] = result_list[pid]
-        final_list.append(product)
+        for pid in result_list.keys():
+            product = {}
+            product['productId'] = pid
+            product['images'] = result_list[pid]
+            final_list.append(product)
 
-    return final_list
+        return final_list
 
-#  TODO: make async requests
-async def check_url(url):
-    valid = False
+    async def __check_url(self, url):
+        valid = False
 
-    if url in cached:
-        return cached[url]
+        if url in self.cached:
+            return self.cached[url]
 
-    r = await http_client.get(url)
-    if r.status < 300:
-        valid = True
-
-    cached[url] = valid
-    return valid
+        r = await self.http_client.get(url)
+        if r.status < 300:
+            valid = True
+        self.cached[url] = valid
+        return valid
 
 async def main():
 
     start = time.time()
 
     file_in = sys.argv[1]
+    file_out = sys.argv[2]
 
     dict_out = [json.loads(line.rstrip('\n')) for line in open(file_in)]
 
-    o = await split_list(dict_out)
-    await http_client.close()
-    final = etl(o)
+    async with Aggregate() as agg:
+        final = await agg.sanitize(dict_out)
+        agg.dump(final, file_out)
 
-    pprint(final)
     end = time.time()
 
     print(f'Elapsed time: {end-start}')
